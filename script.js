7553dd1f02f86d05c13a145ead18f7e602ed89b9
Aadil-7 <aadilbhusnur007@gmail.com>
2022-08-02 20:36:45 +0530

    changed almost all of the JavaScript to make it more object oriented

--- script.js
+++ script.js
@@ -1,81 +1,80 @@
-
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissor = document.getElementById('scissor');
+// ACCESSING ALL THE REQUIRED ELEMENTS
 const result = document.getElementById('result');
 const robosc = document.getElementById('robossc');
-const usrsc = document.getElementById('usrssc')
+const usrsc = document.getElementById('usrssc');
+const rpsValues = ['rock', 'paper', 'scissor']
 
-let userscore = 0;
-let roboscore = 0;
-let userChoice;
+// DECLARING CHOICES OF EACH PLAYER
 let roboChoice;
+let userChoice;
+// GENERATING RANDOM ROBOT CHOICE. 
+const autoChoice = () => {
+    let choice = Math.floor(Math.random() * 3);
+    return rpsValues[choice]
+}
 
-const gameLogic = () => {
-    if(userscore == 10){
+// CREATING BOJECT TO KEEP TRACK OF SCORE.
+const score = {
+    userScore: 0,
+    roboScore: 0
+}
+
+// DECLARING FUNCTIONS TO OBTAIN USER CHOICE AND RUNNING GAME LOGIC AND ALSO UPDATING SCORE.
+const rockVal = () => {
+    roboChoice = autoChoice()
+    userChoice = rpsValues[0];
+    game();
+    scoreUpdate();
+}
+const paperVal = () => {
+    userChoice = rpsValues[1];
+    roboChoice = autoChoice()
+    game();
+    scoreUpdate();
+}
+const scissorVal = () => {
+    userChoice = rpsValues[2];
+    roboChoice = autoChoice()
+    game();
+    scoreUpdate();
+}
+// DECLARING FUNCTION TO UPDATE SCORE.
+const scoreUpdate = () => {
+    robosc.innerText = score["roboScore"]
+    usrsc.innerText = score["userScore"]
+}
+// DECLARING FUNCTION TO RESET GAME.
+const reset = () => {
+    score["roboScore"] = 0;
+    score["userScore"] = 0;
+    robosc.innerText = score["roboScore"]
+    usrsc.innerText = score["userScore"]
+    result.innerText = '';
+}
+
+// DECLARING FUNCTION WHICH CARRYS MAIN LOGIC OF GAME BY COMPARING USER'S CHOICE AND COMPUTER'S CHOICE AND UPDATING SCORE.
+const game = () => {
+    if (score["userScore"] == 5) {
         alert(`Congratulations you won this game.`);
         reset();
-    } else if (roboscore == 10){
+    } else if (score["roboScore"] == 5) {
         alert(`Hmm... You lost, try again.`);
         reset();
     } else {
-    if(userChoice == roboChoice){
-        result.innerText = 'Its a tie!';
-    } else if (userChoice == 0 && roboChoice == 1){
-        result.innerText = 'Robo won!';
-        roboscore += 1; 
-        setScore();
-    } else if(userChoice == 0 && roboChoice == 2) {
-        result.innerText = 'You won!';
-        userscore += 1; 
-        setScore();
-    } else if(userChoice == 1 && roboChoice == 0) {
-        result.innerText = 'You won!';
-        userscore += 1; 
-        setScore();
-    } else if(userChoice == 1 && roboChoice == 2) {
-        result.innerText = 'Robo won!';
-        roboscore += 1; 
-        setScore();
-    } else if(userChoice == 2 && roboChoice == 0) {
-        result.innerText = 'Robo won!';
-        roboscore += 1; 
-        setScore();
-    } else{
-        result.innerText = 'You won!';
-        userscore += 1; 
-        setScore();
+        if (userChoice == roboChoice) {
+            result.innerText = 'Its a tie!';
+        } else if (userChoice == 'rock' && roboChoice == 'scissor') {
+            result.innerText = 'You won!';
+            score["userScore"] += 1;
+        } else if (userChoice == 'paper' && roboChoice == 'rock') {
+            result.innerText = 'You won!';
+            score["userScore"] += 1;
+        } else if (userChoice == 'scissor' && roboChoice == 'paper') {
+            result.innerText = 'You won!';
+            score["userScore"] += 1;
+        } else {
+            result.innerText = 'Robo won!';
+            score["roboScore"] += 1;
+        }
     }
 }
-}
-
-const setScore = () => {
-    robosc.innerText = roboscore;
-    usrsc.innerText = userscore;
-}
-
-const reset = () => {
-    userscore = 0; roboscore=0;
-    robosc.innerText = roboscore;
-    usrsc.innerText = userscore;
-    result.innerText = '';
-}
-const rockVal = () => {
-    userChoice = rock.value
-    roboChoice = Math.floor(Math.random()*3);
-    gameLogic();
-}
-const paperVal = () => {
-    userChoice = paper.value
-    roboChoice = Math.floor(Math.random()*3);
-    gameLogic();
-}
-const scissorVal = () => {
-    userChoice = scissor.value
-    roboChoice = Math.floor(Math.random()*3);
-    gameLogic();
-}
-
-const rspValues = document.querySelectorAll('button');
-
-console.log(rspValues);
